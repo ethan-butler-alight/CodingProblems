f08fda78232dd886d8467c074425ca596c8a901c
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2022-10-21 14:34:19 -0400

    Added someone else's solution to two-sum

--- two-sum.py
+++ two-sum.py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Page: https://leetcode.com/problems/two-sum/
 Prompt:
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -6,13 +6,37 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
-'''
+
+External Solution based off: https://leetcode.com/problems/two-sum/discuss/2361743/Python-Simple-Solution-oror-O(n)-Time-oror-O(n)-Space:
+
+"""
+
 
 class Solution:
+    # My solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
-        for i in range(0, length):            
+        for i in range(0, length):
             for j in range(i, length):
                 if i != j:
-                    if (nums[i]+nums[j]) == target:
+                    if (nums[i] + nums[j]) == target:
                         return i, j
+
+    # Reference cited above
+    def twoSumExternalSolution(nums, target):
+        dictionary = {}
+
+        # i is the index in the list, j is the number in the list
+        for i, j in enumerate(nums):
+            r = target - j
+        # Average time complexity for this accessing an element in a dictionary is O(1).
+        # https://wiki.python.org/moin/TimeComplexity
+
+        # Found the target
+        if r in d:
+
+            # return the index for both elements that add up to the target
+            return [d[r], i]
+        # If r is not in the dictionary, add the current element to the dictionary
+        # {Key (number in the array): Value (index in the array)}
+        d[j] = i
