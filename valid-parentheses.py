fad994e9ca0f28fdbda626c5c94a92de46884a56
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2022-10-27 14:53:12 -0400

    Completed valid parentheses

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,62 +1,35 @@
-def isValid(s):
-
-    stack = list(s)
-    pairs = {"(": ")", "{": "}", "[": "]"}
-    length = len(stack)
-
-    pointer = 1
-
-    while pointer < length:
-        current = stack[pointer]
-
-        if current == pairs[stack[0]]:
-            stack.pop(pointer)
-            stack.pop(0)
-            length = len(stack)
-        else:
-            pointer += 1
-            if pointer == length:
-                return False
-
-    return True
+# Problem: https://leetcode.com/problems/valid-parentheses/
+# Better/Alternative Solution:
+from typing import List
 
 
-print(isValid("([)]"))
+class Solution:
+    def isValid(self, s: str) -> bool:
 
-
-def isValid2(s):
-
-    stack = list(s)
-    pairs = {"(": ")", "{": "}", "[": "]"}
-    length = len(stack)
-
-    while length > 0:
-        if length > 1:
-            current = stack[0]
-            nxt = stack[1]
-        else:
+        if (len(s)) % 2 != 0:
             return False
 
-        if current not in pairs:
-            return False
-        elif nxt == pairs[current]:
-            stack.pop(0)
-            stack.pop(0)
-        else:
-            position = 1
+        stack = []
 
-            while position < length:
-                pointer = stack[position]
-                if pointer == pairs[current]:
-                    stack.pop(position)
-                    stack.pop(0)
-                    break
+        pairs = {"{": "}", "(": ")", "[": "]"}
+
+        for i in s:
+            if i in pairs:
+                stack.append(i)
+            else:
+                if len(stack) > 0:
+                    top = stack.pop()
                 else:
-                    position += 1
-                    if position == length:
-                        return False
+                    return False
 
-        length = len(stack)
-        print("Stack", stack)
+                if pairs.get(top) != i:
+                    return False
 
-    return True
+        if len(stack) > 0:
+            return False
+        else:
+            return True
+
+
+input = "){"
+print(Solution().isValid(input))
